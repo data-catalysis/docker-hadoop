46b12fead471d43c1a0f9cf36986b4d44a992692
data-catalysis <data.catalysis@gmail.com>
2020-01-03 12:24:59 +0700

    update hadoop docker

--- get-sw.sh
+++ get-sw.sh
@@ -10,10 +10,11 @@ APACHE_MIRROR="http://mirror.dsrg.utoronto.ca/apache/"
 
 HADOOP_TARFILE="hadoop-${HADOOP_VERSION}.tar.gz"
 HADOOP_APACHE_PATH="hadoop/common/hadoop-${HADOOP_VERSION}/${HADOOP_TARFILE}"
-#SPARK_TARFILE="spark-${SPARK_VERSION}-bin-hadoop${SPARK_HADOOP_COMPAT}.tgz"
-#SPARK_APACHE_PATH="spark/spark-${SPARK_VERSION}/${SPARK_TARFILE}"
+SPARK_TARFILE="spark-${SPARK_VERSION}-bin-hadoop${SPARK_HADOOP_COMPAT}.tgz"
+SPARK_APACHE_PATH="spark/spark-${SPARK_VERSION}/${SPARK_TARFILE}"
 
-mkdir -p sw
-wget -nc ${APACHE_MIRROR}${HADOOP_APACHE_PATH} -O sw/${HADOOP_TARFILE}
-#wget -nc ${APACHE_MIRROR}${SPARK_APACHE_PATH} -O sw/${SPARK_TARFILE}
+mkdir -p base/sw
+mkdir -p resourcemanager/sw
+wget -nc ${APACHE_MIRROR}${HADOOP_APACHE_PATH} -O base/sw/${HADOOP_TARFILE}
+# wget -nc ${APACHE_MIRROR}${SPARK_APACHE_PATH} -O resourcemanager/sw/${SPARK_TARFILE}
 
--- resourcemanager/run.sh
+++ resourcemanager/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+. "/spark/sbin/spark-config.sh"
+
+. "/spark/bin/load-spark-env.sh"
+
 $HADOOP_PREFIX/bin/yarn --config $HADOOP_CONF_DIR resourcemanager
